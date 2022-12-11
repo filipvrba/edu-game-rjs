94d42cc2dc1f07ff5ed19f47161c7d0f514c0c0b
Filip Vrba <filipvrba@pm.me>
2022-12-11 20:05:46 +0100

    fuck token

--- src/.js/third-side/mruby.js
+++ src/.js/third-side/mruby.js
@@ -4,6 +4,7 @@ export default class MRuby {
   static get_result(mrb_code, callback) {
     let encode_mrb_code = encodeURIComponent(mrb_code);
     let mrb_token = ENV.MRBAPI_TOKEN;
+    console.log(mrb_code);
     let uri = `${ENV.VITE_MRBAPI_URL}?token=${mrb_token}&mrb=${encode_mrb_code}`;
     Net.get_json(uri, data => callback(data.result, data.message))
   }
