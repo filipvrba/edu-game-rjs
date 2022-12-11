e45be00f1794f571586024df48d0ba47558d9355
Filip Vrba <filipvrba@pm.me>
2022-12-11 19:55:13 +0100

    change token

--- src/.js/third-side/mruby.js
+++ src/.js/third-side/mruby.js
@@ -3,7 +3,7 @@ import { ENV } from "../env";
 export default class MRuby {
   static get_result(mrb_code, callback) {
     let encode_mrb_code = encodeURIComponent(mrb_code);
-    let mrb_token = process.env.MRBAPI_TOKEN;
+    let mrb_token = ENV.MRBAPI_TOKEN;
     let uri = `${ENV.VITE_MRBAPI_URL}?token=${mrb_token}&mrb=${encode_mrb_code}`;
     Net.get_json(uri, data => callback(data.result, data.message))
   }
